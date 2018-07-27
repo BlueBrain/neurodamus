85c3c3e38f250a7bd00d088b79552abaa8782989
Fernando Pereira <fernando.pereira@epfl.ch>
2018-07-27 15:33:10 +0200

    Adding an entry-point init.py file, mimicking hoc
    Change-Id: I4e4dd0e58597debe6cb2c957011f6e2aa73b57fb

---
+++ init.py
@@ -0,0 +1,11 @@
+"""
+Neurodamus is a software for handling neuronal simulation using neuron.
+
+Copyright (c) 2018 Blue Brain Project, EPFL.
+All rights reserved
+"""
+from neurodamus import Neurodamus
+
+if __name__ == "__main__":
+    Neurodamus("BlueConfig").run()
+
