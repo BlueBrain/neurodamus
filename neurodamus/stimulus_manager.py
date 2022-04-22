48a823341d657256fcef0c33d0701fd7a5aedfa3
Tharayil Joseph <joseph.tharayil@epfl.ch>
2022-04-22 20:45:11 +0200

    Bug fixes

--- neurodamus/stimulus_manager.py
+++ neurodamus/stimulus_manager.py
@@ -1038,20 +1038,23 @@ class Extracellular(BaseStim):
             if len(self.AmpStart) != len(self.width):
                 raise Exception("Each amplitude must have corresponding width")
 
-        self.ramp_up_number == None
-        self.ramp_down_number == None
+        self.ramp_up_number = None
+        self.ramp_down_number = None
 
         if 'TI' in self.type:
             if stim_info.get("RampUpTime") is not None:
                 ramp_up_time = float(stim_info.get("RampUpTime"))
-                self.ramp_up_number = ramp_up_time/0.025 # Hardcoded dt of 0.025 ms
+                self.ramp_up_number = int(ramp_up_time/0.025) # Hardcoded dt of 0.025 ms
+
 
             if stim_info.get("RampDownTime") is not None:
                 ramp_down_time = float(stim_info.get("RampDownTime"))
-                self.ramp_down_number = ramp_down_time/0.025 # Hardcoded dt of 0.025 ms
+                self.ramp_down_number = int(ramp_down_time/0.025) # Hardcoded dt of 0.025 ms
+
 
             if ramp_up_time+ramp_down_time>self.duration:
                 raise Exception("Ramps must be shorter than the duration")
+
 
 
 
