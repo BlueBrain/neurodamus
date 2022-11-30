ad0058f81cba2df086a3f5a4f5eed211eb50c3df
Katta <alessandro.cattabiani@epfl.ch>
2022-11-30 16:23:33 +0100

    add ngv pytest WIP

--- tests/test_ngv.py
+++ tests/test_ngv.py
@@ -3,9 +3,13 @@ Test suite for Neurodamus NGV support
 """
 import os.path as osp
 
-
-if __name__ == "__main__":
+def test_loading_and_run():
+    """Base testing: we instantiate neurodamus and run a simple simulation with ngv stuff"""
     from neurodamus import Neurodamus
     blueconfig = osp.join(osp.abspath(osp.dirname(__file__)),
                           "simulations", "ngv", "BlueConfig")
-    Neurodamus(blueconfig, logging_level=2).run()
+    ndam = Neurodamus(blueconfig, logging_level=2)
+    ndam.run()
+
+if __name__ == "__main__":
+    test_loading_and_run()
