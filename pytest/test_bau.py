7688d1b53ef2d4be298ecec112720f25ff585516
Katta <alessandro.cattabiani@epfl.ch>
2022-12-12 16:41:12 +0100

    WIP

---
+++ pytest/test_bau.py
@@ -0,0 +1,27 @@
+"""
+Test suite for Neurodamus NGV support
+"""
+import os
+import os.path as osp
+import pytest
+
+
+@pytest.mark.slow
+@pytest.mark.forked
+# @pytest.mark.skipif(
+#     not os.path.isfile("/gpfs/bbp.cscs.ch/home/cattabia/subcellular/multiscale_run/sonata/vasculature.h5"),
+#     reason="vasculature file not available")
+@pytest.mark.skipif(
+    not os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"),
+    reason="Test requires loading a neocortex model to run")
+def test_loading_and_run():
+    pass
+    # """Base testing: we instantiate neurodamus and run a simple simulation with ngv stuff"""
+    # from neurodamus import Neurodamus
+    # blueconfig = osp.join(osp.abspath(osp.dirname(__file__)),
+    #                       "simulations", "ngv", "BlueConfig")
+    # ndam = Neurodamus(blueconfig, logging_level=2)
+    # ndam.run(2)
+
+if __name__ == "__main__":
+    test_loading_and_run()
