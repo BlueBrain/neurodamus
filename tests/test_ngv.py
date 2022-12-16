2ab700b6efe150b2c1fee65aa1771c5474bba6ef
Katta <alessandro.cattabiani@epfl.ch>
2022-12-16 10:46:50 +0100

    revert test

--- tests/test_ngv.py
+++ tests/test_ngv.py
@@ -1,23 +1,11 @@
 """
 Test suite for Neurodamus NGV support
 """
-import os
 import os.path as osp
-import pytest
 
 
-@pytest.mark.slow
-@pytest.mark.forked
-@pytest.mark.skipif(
-    not os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"),
-    reason="Test requires loading a neocortex model to run")
-def test_loading_and_run():
-    """Base testing: we instantiate neurodamus and run a simple simulation with ngv stuff"""
+if __name__ == "__main__":
     from neurodamus import Neurodamus
     blueconfig = osp.join(osp.abspath(osp.dirname(__file__)),
                           "simulations", "ngv", "BlueConfig")
-    ndam = Neurodamus(blueconfig, logging_level=2)
-    ndam.solve(2)
-
-if __name__ == "__main__":
-    test_loading_and_run()
+    Neurodamus(blueconfig, logging_level=2).run()
