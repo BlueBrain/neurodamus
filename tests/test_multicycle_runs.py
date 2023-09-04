a883a5a3057346aa3b8e63f3a8c901e8d48748a0
Ioannis Magkanaris <iomagkanaris@gmail.com>
2023-09-04 16:45:52 +0200

    Revert "Skip CORENEURON unit tests pending for a fix from NEURON (#38)"
    This reverts commit 979383e1e172ff3230eec5bef920efe3e4413bed.
    
    NEURON_BRANCH=jblanco/coreneuron_from_neurodamus

--- tests/test_multicycle_runs.py
+++ tests/test_multicycle_runs.py
@@ -92,11 +92,9 @@ def _read_sonata_spike_file(spike_file):
 
 
 @pytest.mark.forked
-# @pytest.mark.skipif(
-#     not os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"),
-#     reason="Test requires loading a neocortex model to run")
-@pytest.mark.skip(
-    reason="Pending for a NEURON fix regarding the dynapmic MPI library")
+@pytest.mark.skipif(
+    not os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"),
+    reason="Test requires loading a neocortex model to run")
 def test_v5_sonata_multisteps():
     import numpy.testing as npt
     from neurodamus import Neurodamus
@@ -126,11 +124,9 @@ def test_v5_sonata_multisteps():
 
 
 @pytest.mark.forked
-# @pytest.mark.skipif(
-#     not os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"),
-#     reason="Test requires loading a neocortex model to run")
-@pytest.mark.skip(
-    reason="Pending for a NEURON fix regarding the dynapmic MPI library")
+@pytest.mark.skipif(
+    not os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"),
+    reason="Test requires loading a neocortex model to run")
 def test_usecase3_sonata_multisteps():
     import numpy.testing as npt
     from neurodamus import Neurodamus
