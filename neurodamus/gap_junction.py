4ccc2831a4d4678a53008188f36b1d339c52c02f
Weina Ji <weina.ji@epfl.ch>
2023-09-11 15:22:17 +0200

    fix flake8

--- neurodamus/gap_junction.py
+++ neurodamus/gap_junction.py
@@ -87,7 +87,7 @@ class GapJunctionManager(ConnectionManagerBase):
 
         super().__init__(gj_conf, target_manager, cell_manager, src_cell_manager, **kw)
         self._src_target_filter = target_manager.get_target(cell_manager.circuit_target,
-                                                            target_pop=src_cell_manager.population_name)
+                                                            src_cell_manager.population_name)
 
     def open_synapse_file(self, synapse_file, *args, **kw):
         super().open_synapse_file(synapse_file, *args, **kw)
