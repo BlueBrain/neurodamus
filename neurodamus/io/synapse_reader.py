3b0bb948e21085499aa8ad936f46b83ae31cfca7
Luc Grosheintz <luc.grosheintz@gmail.ch>
2023-11-22 16:14:03 +0100

    Make neurodamus collective.
    This commit prepares neurodamus for the upcoming support of MPI
    in libsonata.

--- neurodamus/io/synapse_reader.py
+++ neurodamus/io/synapse_reader.py
@@ -232,7 +232,8 @@ class SonataReader(SynapseReader):
     }
 
     def _open_file(self, src, population, _):
-        storage = libsonata.EdgeStorage(src)
+        hdf5_reader = libsonata.make_collective_reader(False, True)
+        storage = libsonata.EdgeStorage(src, hdf5_reader=hdf5_reader)
         if not population:
             assert len(storage.population_names) == 1
             population = next(iter(storage.population_names))
