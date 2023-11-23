6f8be38fe9eb88ad6bddd1f63aba0217716fd25c
Fernando Pereira <fernando.pereira@epfl.ch>
2023-11-23 16:28:44 +0100

    Loose comparison of dry_run value which would seldom fail

--- tests/scientific/test_dry_run_worflow.py
+++ tests/scientific/test_dry_run_worflow.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from collections import Counter
 
 USECASE3 = Path(__file__).parent.absolute() / "usecase3"
 
@@ -19,10 +18,12 @@ def test_dry_run_workflow():
 
     assert 20.0 <= nd._dry_run_stats.cell_memory_total <= 30.0
     assert 0.0 <= nd._dry_run_stats.synapse_memory_total <= 1.0
-    assert 90.0 <= nd._dry_run_stats.base_memory <= 120.0
-    expected_items = Counter([('L4_PC-dSTUT', 2),
-                              ('L4_MC-dSTUT', 1),
-                              ('L4_MC-dNAC', 1),
-                              ('L5_PC-dSTUT', 1)])
-    assert set(nd._dry_run_stats.metype_counts.items()) == set(expected_items)
+    assert 80.0 <= nd._dry_run_stats.base_memory <= 120.0
+    expected_items = {
+        'L4_PC-dSTUT': 2,
+        'L4_MC-dSTUT': 1,
+        'L4_MC-dNAC': 1,
+        'L5_PC-dSTUT': 1
+    }
+    assert nd._dry_run_stats.metype_counts == expected_items
     assert nd._dry_run_stats.suggest_nodes(0.3) > 0
