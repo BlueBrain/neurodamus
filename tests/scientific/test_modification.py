3400b82f2c4e89d1d553d3b9b375a9bf90b8ff75
Weina Ji <weina.ji@epfl.ch>
2023-12-14 15:52:25 +0100

    migrate modification test to use SONATA config files

--- tests/scientific/test_modification.py
+++ tests/scientific/test_modification.py
@@ -1,104 +1,12 @@
-import os
-import pytest
-from tempfile import NamedTemporaryFile
-
-from neurodamus.utils import compat
+from pathlib import Path
 from neurodamus.utils.logging import log_verbose
 # !! NOTE: Please dont import Neuron or Nd objects. pytest will trigger Neuron instantiation!
 
-
-# BlueConfig string
-BC_str = """
-Run Default
-{{
-    CellLibraryFile /gpfs/bbp.cscs.ch/project/proj83/circuits/Bio_M/20200805/circuit.mvd3
-    nrnPath <NONE>
-    CircuitPath .
-    TargetFile {target_file}
-
-    BioName /gpfs/bbp.cscs.ch/project/proj83/circuits/Bio_M/20200805/bioname
-    Atlas /gpfs/bbp.cscs.ch/project/proj83/data/atlas/S1/MEAN/P14-MEAN
-
-    METypePath /gpfs/bbp.cscs.ch/project/proj83/singlecell/release_2020-07-31/hoc
-    MEComboInfoFile /gpfs/bbp.cscs.ch/project/proj83/singlecell/fixed_L6_allBPC_thresholds/\
-mecombo_emodel.tsv
-    MorphologyPath /gpfs/bbp.cscs.ch/project/proj83/morphologies/fixed_ais_L23PC_20201210/ascii
-    MorphologyType asc
-
-    CurrentDir .
-    OutputRoot .
-
-    CircuitTarget single
-    Duration 200
-    Dt 0.025
-
-    RNGMode Random123
-    BaseSeed 549821
-
-    Simulator NEURON
-    RunMode WholeCell
-    ForwardSkip 0
-
-    ExtracellularCalcium 1.20
-    MinisSingleVesicle 1
-    SpikeLocation AIS
-    V_Init -80
-}}
-
-Stimulus stimTest
-{{
-    Mode Current
-    Pattern RelativeLinear
-    Delay 0
-    Duration 200
-    PercentStart 120
-}}
-
-StimulusInject stimTestInject
-{{
-    Stimulus stimTest
-    Target single
-}}
-
-Stimulus hypamp
-{{
-        Mode Current
-     Pattern Hyperpolarizing
-       Delay 0.0
-    Duration 200
-}}
-
-StimulusInject hypampInject
-{{
-    Stimulus hypamp
-      Target single
-}}
-"""
-
-# Target file string
-TGT_str = """
-Target Cell single
-{
-    a3899539
-}
-"""
+SIM_DIR = Path(__file__).parent.parent.absolute() / "simulations" / "v5_sonata"
+CONFIG_FILE_MINI = SIM_DIR / "simulation_config_mini.json"
 
 
-@pytest.fixture(scope="module")
-def blueconfig():
-    # dump config to files
-    with NamedTemporaryFile("w", prefix='test_ttx_mod_tgt', delete=False) as tgt_file:
-        tgt_file.write(TGT_str)
-    with NamedTemporaryFile("w", prefix="test_ttx_mod_bc", delete=False) as bc_file:
-        bc_file.write(BC_str.format(target_file=tgt_file.name))
-
-    yield bc_file.name
-
-    os.unlink(bc_file.name)
-    os.unlink(tgt_file.name)
-
-
-def test_TTX_modification(blueconfig):
+def test_TTX_modification():
     """
     A test of enabling TTX with a short simulation.
     Expected outcome is non-zero spike count without TTX, zero with TTX.
@@ -110,7 +18,7 @@ def test_TTX_modification(blueconfig):
     from neurodamus.node import Node
 
     GlobalConfig.verbosity = LogLevel.VERBOSE
-    n = Node(blueconfig)
+    n = Node(str(CONFIG_FILE_MINI))
 
     # setup sim
     n.load_targets()
@@ -123,13 +31,8 @@ def test_TTX_modification(blueconfig):
     nspike_noTTX = sum(len(spikes) for spikes, _ in n._spike_vecs)
 
     # append modification to config directly
-    TTX_mod = compat.Map(Nd.Map())
-    TTX_mod["Type"] = "TTX"
-    TTX_mod["Target"] = "single"
-    # set HOC map as value of key "applyTTX"
-    SimConfig.modifications.hoc_map.put("applyTTX", TTX_mod.hoc_map)
-    # manually update item count in compat.Map
-    SimConfig.modifications._size = int(SimConfig.modifications.hoc_map.count())
+    TTX_mod = {"Type": "TTX", "Target": "Mini5"}
+    SimConfig.modifications["applyTTX"] = TTX_mod
 
     # setup sim again
     Nd.t = 0.0
@@ -144,7 +47,7 @@ def test_TTX_modification(blueconfig):
     assert nspike_TTX == 0
 
 
-def test_ConfigureAllSections_modification(blueconfig):
+def test_ConfigureAllSections_modification():
     """
     A test of performing ConfigureAllSections with a short simulation.
     Expected outcome is higher spike count when enabled.
@@ -156,7 +59,7 @@ def test_ConfigureAllSections_modification(blueconfig):
     from neurodamus.node import Node
 
     GlobalConfig.verbosity = LogLevel.VERBOSE
-    n = Node(blueconfig)
+    n = Node(str(CONFIG_FILE_MINI))
 
     # setup sim
     n.load_targets()
@@ -164,25 +67,21 @@ def test_ConfigureAllSections_modification(blueconfig):
     n.create_synapses()
     n.enable_stimulus()
     n.sim_init()
-    n.solve()
+    n.solve(tstop=150)  # longer duration to see influence on spikes
     nspike_noConfigureAllSections = sum(len(spikes) for spikes, _ in n._spike_vecs)
 
     # append modification to config directly
-    ConfigureAllSections_mod = compat.Map(Nd.Map())
-    ConfigureAllSections_mod["Type"] = "ConfigureAllSections"
-    ConfigureAllSections_mod["Target"] = "single"
-    ConfigureAllSections_mod["SectionConfigure"] = "%s.gSK_E2bar_SK_E2 = 0"
-    # set HOC map as value of key "no_SK_E2"
-    SimConfig.modifications.hoc_map.put("no_SK_E2", ConfigureAllSections_mod.hoc_map)
-    # manually update item count in compat.Map
-    SimConfig.modifications._size = int(SimConfig.modifications.hoc_map.count())
+    ConfigureAllSections_mod = {"Type": "ConfigureAllSections",
+                                "Target": "Mini5",
+                                "SectionConfigure": "%s.gSK_E2bar_SK_E2 = 0"}
+    SimConfig.modifications["no_SK_E2"] = ConfigureAllSections_mod
 
     # setup sim again
     Nd.t = 0.0
     n._sim_ready = False
     n.enable_modifications()
     n.sim_init()
-    n.solve()
+    n.solve(tstop=150)
     nspike_ConfigureAllSections = sum(len(spikes) for spikes, _ in n._spike_vecs)
 
     log_verbose("spikes without ConfigureAllSections = %s, with ConfigureAllSections = %s",
