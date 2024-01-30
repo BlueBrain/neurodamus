0288c6cc8be9f114326b9c0a0f87cb8cc42161db
Alexander Temerev <alexander.temerev@epfl.ch>
2024-01-30 16:59:27 +0100

    Works with early arguments now (including early config file)

--- neurodamus/utils/cli.py
+++ neurodamus/utils/cli.py
@@ -1,17 +1,24 @@
-
-
 def extract_arguments(args):
     """Get the options for neurodamus and launch it.
 
     We can't use positional arguments with special so we look for
     --configFile=FILE, which defaults to simulation_config.json
     """
+    first_argument_pos = 1
+    init_py_reached = False
     config_file = "simulation_config.json"
-    for i, arg in enumerate(args[1:]):
-        if not arg.startswith("-"):
-            raise ValueError("Positional arguments are not supported")
-        elif arg.startswith("--configFile="):
+    for i, arg in enumerate(args):
+        if arg.startswith("--configFile="):
             config_file = arg.split('=')[1]
-            break
-    args = [config_file] + [x for x in args[1:] if not x.startswith("--configFile=")]
-    return args
+            continue
+        if not init_py_reached:
+            if arg.endswith("init.py"):
+                first_argument_pos = i + 1
+                init_py_reached = True
+            continue
+        elif not arg.startswith("-"):
+            raise ValueError("Positional arguments are not supported")
+
+    result_args = ([config_file] +
+                   [x for x in args[first_argument_pos:] if not x.startswith("--configFile=")])
+    return result_args
--- tests/unit/test_init_py_config.py
+++ tests/unit/test_init_py_config.py
@@ -5,25 +5,25 @@ from neurodamus.utils.cli import extract_arguments
 _default_config_file = 'simulation_config.json'
 
 
-def test_init_empty(_mock_neuron):
+def test_init_empty():
     args = extract_arguments(['some/dir/init.py'])
     assert len(args) == 1
     assert args[0] == _default_config_file
 
 
-def test_init_non_positional(_mock_neuron):
+def test_init_non_positional():
     with pytest.raises(ValueError):
         extract_arguments(['init.py', 'simulation_config.json'])
 
 
-def test_init_config_only(_mock_neuron):
+def test_init_config_only():
     args = extract_arguments(['another/dir/init.py',
                               '--configFile=conf/my_config.json'])
     assert len(args) == 1
     assert args[0] == 'conf/my_config.json'
 
 
-def test_init_pass_options(_mock_neuron):
+def test_init_pass_options():
     args = extract_arguments(['another/dir/init.py', '--foo=bar',
                               '--configFile=conf/my_config.json',
                               '-v', '--baz=qux'])
@@ -32,3 +32,23 @@ def test_init_pass_options(_mock_neuron):
     assert args[1] == '--foo=bar'
     assert args[2] == '-v'
     assert args[3] == '--baz=qux'
+
+
+def test_init_first_args():
+    args = extract_arguments(['dplace', 'special' 'another/dir/init.py', '--foo=bar',
+                              '--configFile=conf/my_config.json',
+                              '-v', '--baz=qux'])
+    assert len(args) == 4
+    assert args[0] == 'conf/my_config.json'
+    assert args[1] == '--foo=bar'
+    assert args[2] == '-v'
+    assert args[3] == '--baz=qux'
+
+
+def test_init_early_config_file():
+    args = extract_arguments(['dplace', 'special', '--configFile=my_config.json', '--some=pre',
+                              'etc', 'dir/init.py', '--foo=bar', '-v'])
+    assert len(args) == 3
+    assert args[0] == 'my_config.json'
+    assert args[1] == '--foo=bar'
+    assert args[2] == '-v'
