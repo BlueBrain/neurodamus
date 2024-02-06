1451fb62bf808464c6d05cdcaab7b65c2f4b7f57
Alexander Temerev <alexander.temerev@epfl.ch>
2024-02-06 10:52:10 +0100

    Integration test for erroneous HOC synapse config which is supposed to fail

--- tests/integration-e2e/test_connection.py
+++ tests/integration-e2e/test_connection.py
@@ -6,6 +6,7 @@ from neurodamus.node import Node
 
 SIM_DIR = Path(__file__).parent.parent.absolute() / "simulations" / "v5_sonata"
 CONFIG_FILE_MINI = SIM_DIR / "simulation_config_mini.json"
+CONFIG_FILE_ERR = SIM_DIR / "simulation_config_err.json"
 
 
 @pytest.mark.slow
@@ -26,3 +27,19 @@ def test_add_synapses():
     assert len(conn._synapse_params) == n_syns + 1
     for syn_manager in n._circuits.all_synapse_managers():
         syn_manager.finalize(0, False)
+
+
+@pytest.mark.slow
+def test_hoc_config_error():
+    with pytest.raises(ValueError):
+        n = Node(str(CONFIG_FILE_ERR))
+        n.load_targets()
+        n.create_cells()
+        n.create_synapses()
+        syn_manager = n.circuits.get_edge_manager("default", "default")
+        conn = list(syn_manager.get_connections(62798))[0]
+        new_params = SynapseParameters.create_array(1)
+        new_params[0].sgid = conn.sgid
+        conn.add_synapses(n._target_manager, new_params)
+        for syn_manager in n._circuits.all_synapse_managers():
+            syn_manager.finalize(0, False)
