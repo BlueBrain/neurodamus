1ac4668804a931c1e909a4aa2510f1d92a669710
Jorge Blanco Alonso <jorge.blancoalonso@epfl.ch>
2024-02-27 15:45:26 +0100

    Move CompartmentMapping to python

--- neurodamus/core/coreneuron_configuration.py
+++ neurodamus/core/coreneuron_configuration.py
@@ -5,6 +5,104 @@ from ._utils import run_only_rank0
 from . import NeurodamusCore as Nd
 
 
+class CompartmentMapping:
+    """ Interface to register section segment mapping with NEURON.
+    """
+    def __init__(self, cell_distributor):
+        self.cell_distributor = cell_distributor
+        self.pc = Nd.ParallelContext()
+
+    def get_section_index(self, cell, section):
+        """
+        Calculate the global index of a given section within its cell.
+        :param cell: The cell instance containing the section of interest
+        :param section: The specific section for which the index is required
+        :return: The global index of the section, applicable for neuron mapping
+        """
+        section_name = str(section)
+        base_offset = 0
+        section_index = 0
+        if "soma" in section_name:
+            pass  # base_offset is 0
+        elif "axon" in section_name:
+            base_offset = cell.nSecSoma
+        elif "dend" in section_name:
+            base_offset = cell.nSecSoma + cell.nSecAxonalOrig
+        elif "apic" in section_name:
+            base_offset = cell.nSecSoma + cell.nSecAxonalOrig + cell.nSecBasal
+        elif "ais" in section_name:
+            base_offset = cell.nSecSoma + cell.nSecAxonalOrig + cell.nSecBasal + cell.nSecApical
+        elif "node" in section_name:
+            base_offset = cell.nSecSoma + cell.nSecAxonalOrig + cell.nSecBasal + cell.nSecApical \
+                        + getattr(cell, 'nSecLastAIS', 0)
+        elif "myelin" in section_name:
+            base_offset = cell.nSecSoma + cell.nSecAxonalOrig + cell.nSecBasal + cell.nSecApical \
+                        + getattr(cell, 'nSecLastAIS', 0) + getattr(cell, 'nSecNodal', 0)
+
+        # Extract the index from the section name
+        try:
+            index_str = section_name.split('[')[-1].rstrip(']')
+            section_index = int(index_str)
+        except ValueError:
+            logging.warning(f"Error while getting section index {index_str}")
+
+        return int(base_offset + section_index)
+
+    def create_section_vectors(self, section_id, section, secvec, segvec):
+        num_segments = 0
+        for seg in section:
+            secvec.append(section_id)
+            segvec.append(seg.node_index())
+            num_segments += 1
+
+        return num_segments
+
+    def process_section(self, cell, sections, num_electrodes, all_lfp_factors, section_offset):
+        secvec, segvec, lfp_factors = Nd.Vector(), Nd.Vector(), Nd.Vector()
+        num_segments = 0
+        section_attr = getattr(cell, sections[0], None)
+        if section_attr:
+            for sec in section_attr:
+                section_index = self.get_section_index(cell, sec)
+                num_segments += self.create_section_vectors(section_index, sec, secvec, segvec)
+
+        if num_electrodes > 0 and all_lfp_factors.size() > 0 and num_segments > 0:
+            start_idx = section_offset * num_electrodes
+            end_idx = (section_offset + num_segments) * num_electrodes - 1
+            lfp_factors.copy(all_lfp_factors, start_idx, end_idx)
+
+        self.pc.nrnbbcore_register_mapping(cell.gid, sections[1], secvec, segvec,
+                                           lfp_factors, num_electrodes)
+        return num_segments
+
+    def register_mapping(self):
+        sections = [
+            ('somatic', 'soma'),
+            ('axonal', 'axon'),
+            ('basal', 'dend'),
+            ('apical', 'apic'),
+            ('AIS', 'ais'),
+            ('nodal', 'node'),
+            ('myelinated', 'myelin')
+        ]
+        gidvec = self.cell_distributor.getGidListForProcessor()
+        for activegid in gidvec:
+            cellref = self.cell_distributor.getCell(activegid)
+            all_lfp_factors = Nd.Vector()
+            num_electrodes = 0
+            lfp_manager = getattr(self.cell_distributor, "_lfp_manager", None)
+            if lfp_manager:
+                pop_info = self.cell_distributor.getPopulationInfo(activegid)
+                num_electrodes = lfp_manager.get_number_electrodes(activegid, pop_info)
+                all_lfp_factors = lfp_manager.read_lfp_factors(activegid, pop_info)
+
+            section_offset = 0
+            for section in sections:
+                processed_segments = self.process_section(cellref, section, num_electrodes,
+                                                          all_lfp_factors, section_offset)
+                section_offset += processed_segments
+
+
 class _CoreNEURONConfig(object):
     """
     The CoreConfig class is responsible for managing the configuration of the CoreNEURON simulation.
