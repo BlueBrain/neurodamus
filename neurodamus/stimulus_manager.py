73d945d1a17601c70d55eb59728dddf86abc816e
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-02-27 11:32:30 +0100

    Replaces SEClamp with new conductanceSource point process

--- neurodamus/stimulus_manager.py
+++ neurodamus/stimulus_manager.py
@@ -753,7 +753,7 @@ class SEClamp(BaseStim):
                     continue
 
                 # create single electrode voltage clamp at location
-                seclamp = Nd.h.SEClamp(tpoint_list.x[sec_id], sec=sc.sec)
+                seclamp = Nd.h.conductanceSource(tpoint_list.x[sec_id], sec=sc.sec)
                 seclamp.rs = self.rs
                 seclamp.dur1 = self.duration
                 seclamp.amp1 = self.vhold
