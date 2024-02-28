62fd6e2b549eda08353b2b46b1c12652e5e72fd8
Jorge Blanco Alonso <jorge.blancoalonso@epfl.ch>
2024-02-28 11:18:53 +0100

    Fix flake8 complains

--- neurodamus/core/stimuli.py
+++ neurodamus/core/stimuli.py
@@ -356,7 +356,7 @@ class SignalSource:
 
     @classmethod
     def ornstein_uhlenbeck(cls, tau, sigma, mean, duration, dt=0.25, base_amp=.0, **kw):
-        return cls(base_amp, **kw).add_ornstein_uhlenbeck(tau, sigma, mean,duration, dt)
+        return cls(base_amp, **kw).add_ornstein_uhlenbeck(tau, sigma, mean, duration, dt)
 
     # Operations
     def __add__(self, other):
@@ -434,9 +434,8 @@ class ConductanceSource(SignalSource):
         def __init__(self, cell_section, position=0.5, clamp_container=None,
                      stim_vec_mode=True, time_vec=None, stim_vec=None,
                      reversal=0.0, **clamp_params):
-            
-            
-            # Check sif new conductanceSource mechanism is available
+
+            # Checks if new conductanceSource mechanism is available
             mt = Neuron.h.MechanismType(1)
             mname = Neuron.h.ref('')
             mList = []
@@ -444,11 +443,9 @@ class ConductanceSource(SignalSource):
                 mt.select(i)
                 mt.selected(mname)
                 mList.append(mname[0])
-                                
-            if 'conductanceSource' in mList:
 
+            if 'conductanceSource' in mList:
                 self.clamp = Neuron.h.conductanceSource(position, sec=cell_section)
-            
             else:
                 self.clamp = Neuron.h.SEClamp(position, sec=cell_section)
 
