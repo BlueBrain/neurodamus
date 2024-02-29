c4a87c1d39b9c80484fd07e70ec4c2edf55e5a31
Jorge Blanco Alonso <41900536+jorblancoa@users.noreply.github.com>
2024-02-29 10:04:47 +0100

    [BBPBGLIB-1134] Move ShowProgress.hoc to python (#131)
    ## Context
    Addresses BBPBGLIB-1134.
    Neuron simulation progress hoc file was moved to python.
    
    ## Review
    * [x] PR description is complete
    * [x] Coding style (imports, function length, New functions, classes or
    files) are good
    * [ ] Unit/Scientific test added
    * [ ] Updated Readme, in-code, developer documentation

--- neurodamus/node.py
+++ neurodamus/node.py
@@ -13,7 +13,7 @@ from os import path as ospath
 from collections import namedtuple, defaultdict
 from contextlib import contextmanager
 
-from .core import MPI, mpi_no_errors, return_neuron_timings, run_only_rank0
+from .core import MPI, mpi_no_errors, return_neuron_timings, run_only_rank0, SimulationProgress
 from .core import NeurodamusCore as Nd
 from .core.configuration import CircuitConfig, Feature, GlobalConfig, SimConfig
 from .core._engine import EngineBase
@@ -1301,8 +1301,8 @@ class Node:
     # -
     @return_neuron_timings
     def _run_neuron(self):
-        Nd.load_hoc("ShowProgress")  # TODO: Drop this
-        _ = Nd.ShowProgress(Nd.cvode, MPI.rank)
+        if MPI.rank == 0:
+            _ = SimulationProgress()
         self.solve()
         logging.info("Simulation finished.")
 
