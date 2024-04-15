bbc107920e25db274bb13ec270c81a7ff234e7c2
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-04-15 22:59:11 +0200

    Fixes bugs in stimulus test

--- tests/scientific/test_stimulus_injection.py
+++ tests/scientific/test_stimulus_injection.py
@@ -12,7 +12,7 @@ def _read_sonata_soma_report(report_name):
     pop_name = report.get_population_names()[0]
     ids = report[pop_name].get_node_ids()
     data = report[pop_name].get(node_ids=[ids[0]])
-    return numpy.array(data.data).flatten().tolist()
+    return numpy.array(data.data).flatten()
 
 def test_current_injection_coreneuron():
 
@@ -22,7 +22,7 @@ def test_current_injection_coreneuron():
     config_file = str(SIM_DIR / "simulation_config_iclamp.json")
     os.chdir(SIM_DIR)
     nd = Neurodamus(config_file, disable_reports=False, simulator="CORENEURON",
-                    output_path="output_coreneuron")
+                    output_path="output_coreneuron_iclamp")
 
     nd.run()
 
@@ -30,11 +30,11 @@ def test_current_injection_coreneuron():
     voltage_vec_iclamp = _read_sonata_soma_report(soma_report_path)
     #
     config_file = str(SIM_DIR / "simulation_config_membranecurrentsource.json")
-    nd = Neurodamus(config_file, disable_reports=False, simulator="CORENEURON",
-                    output_path="output_coreneuron")
-    nd.run()
+    nd2 = Neurodamus(config_file, disable_reports=False, simulator="CORENEURON",
+                    output_path="output_coreneuron_membranecurrentsource")
+    nd2.run()
 
-    soma_report_path = os.path.join(nd._run_conf["OutputRoot"], "voltage.h5")
+    soma_report_path = os.path.join(nd2._run_conf["OutputRoot"], "voltage.h5")
     voltage_vec_membranecurrentsource = _read_sonata_soma_report(soma_report_path)
 
     numpy.testing.assert_equal(voltage_vec_iclamp,voltage_vec_membranecurrentsource)
@@ -47,18 +47,18 @@ def test_conductance_injection_coreneuron():
     config_file = str(SIM_DIR / "simulation_config_seclamp.json")
     os.chdir(SIM_DIR)
     nd = Neurodamus(config_file, disable_reports=False, simulator="CORENEURON",
-                    output_path="output_coreneuron")
+                    output_path="output_coreneuron_seclamp")
     nd.run()
 
     soma_report_path = os.path.join(nd._run_conf["OutputRoot"], "voltage.h5")
     voltage_vec_seclamp = _read_sonata_soma_report(soma_report_path)
 
     config_file = str(SIM_DIR / "simulation_config_conductancesource.json")
-    nd = Neurodamus(config_file, disable_reports=False, simulator="CORENEURON",
-                    output_path="output_coreneuron")
-    nd.run()
+    nd2 = Neurodamus(config_file, disable_reports=False, simulator="CORENEURON",
+                    output_path="output_coreneuron_conductancesource")
+    nd2.run()
 
-    soma_report_path = os.path.join(nd._run_conf["OutputRoot"], "voltage.h5")
+    soma_report_path = os.path.join(nd2._run_conf["OutputRoot"], "voltage.h5")
     voltage_vec_conductancesource = _read_sonata_soma_report(soma_report_path)
 
     numpy.testing.assert_equal(voltage_vec_seclamp,voltage_vec_conductancesource)
