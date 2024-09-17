Fix broadcast corruption and improve caching mech

neurodamus/utils/memory.py
@@ -229,7 +229,6 @@ def import_metype_memory_usage(memory_per_metype_file):
     return memory_per_metype
 
 
-@run_only_rank0
 def import_allocation_stats(filename, cycle_i=0) -> dict:
     """
     Import allocation dictionary from serialized pickle file.
@@ -240,19 +239,21 @@ def import_allocation_stats(filename, cycle_i=0) -> dict:
         """Converts an object containing defaultdicts of Vectors to standard Python types."""
         result = {}
         for population, vectors in obj.items():
-            result[population] = {key: np.array(vector) for key,
-                                  vector in vectors.items() if key[1] == cycle_i}
+            result[population] = {
+                key: np.array(vector)
+                for key, vector in vectors.items()
+                if key[1] == cycle_i and key[0] == MPI.rank}
         return result
 
     if _alloc_cache is None:
         logging.warning("Loading allocation stats from %s...", filename)
         with gzip.open(filename, 'rb') as f:
             data = pickle.load(f)
-        _alloc_cache = data
+        _alloc_cache = convert_to_standard_types(data)
     else:
         logging.warning("Using cached allocation stats.")
 
-    return convert_to_standard_types(_alloc_cache)
+    return _alloc_cache
 
 
 @run_only_rank0