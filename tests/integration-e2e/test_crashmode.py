059a71a629091fe123e9b39caabea37a11dfc94f
Jorge Blanco Alonso <jorge.blancoalonso@epfl.ch>
2024-09-30 11:02:51 +0200

    Fix crash test

--- tests/integration-e2e/test_crashmode.py
+++ tests/integration-e2e/test_crashmode.py
@@ -1,6 +1,3 @@
-import json
-import os
-import pytest
 
 
 def test_crash_test_loading(SIM_DIR, tmp_path):
@@ -9,28 +6,14 @@ def test_crash_test_loading(SIM_DIR, tmp_path):
     from neurodamus.metype import PointCell
     from neuron import nrn
     sim_dir = SIM_DIR / "v5_sonata"
-    sim_config_path = sim_dir / "simulation_config.json"
+    sim_config_path = sim_dir / "simulation_config_mini.json"
 
-    with open(sim_config_path) as config_fp:
-        config = json.load(config_fp)
-
-    # Replace target with a large one (18k cells)
-    # Test runs in a single rank and its still fast in crash-test mode
-    new_config_path = tmp_path / "simulation_config.json"
-    config["node_set"] = "L4_SP"
-    with open(new_config_path, "w") as new_config:
-        json.dump(config, new_config)
-
-    # Symlink aux files, searched in the same dir
-    for filename in ("circuit_config.json", "node_sets.json"):
-        os.symlink(sim_dir / filename, tmp_path / filename)
-
-    n = Node(str(new_config_path), {"crash_test": True})
+    n = Node(str(sim_config_path), {"crash_test": True})
     n.load_targets()
     n.create_cells()
 
     cell_manager: CellDistributor = n.circuits.get_node_manager("default")
-    assert len(cell_manager.cells) == 7687
+    assert len(cell_manager.cells) == 5
     cell0 = next(iter(cell_manager.cells))
     assert isinstance(cell0, PointCell)
     assert isinstance(cell0.soma, list)
