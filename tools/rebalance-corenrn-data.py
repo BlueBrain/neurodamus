8bf789f03e9ceb5523c98d8ebebacabe8e60817f
Fernando Pereira <fernando.pereira@epfl.ch>
2024-10-24 16:16:48 +0200

    Better distribution using knapsack. New option: --histogram

--- tools/rebalance-corenrn-data.py
+++ tools/rebalance-corenrn-data.py
@@ -16,14 +16,14 @@ import math
 import os
 import sys
 
+# Numpy may be required (histogram)
+numpy = None
 
-def distribute_dat_to_bucket(dat_entry, buckets, bucket_sizes, base_dir="."):
+
+def distribute_dat_to_bucket(dat_entry, size, buckets, bucket_sizes, base_dir="."):
     """
     Distribute a single file into the bucket with the least total size.
     """
-    dat_file = f"{dat_entry}_2.dat"
-    file_path = os.path.join(base_dir, dat_file)
-    size = os.path.getsize(file_path)
     # Pop the bucket with the smallest size
     smallest_size, smallest_bucket_index = heapq.heappop(bucket_sizes)
     # Assign the file to this bucket
@@ -33,7 +33,7 @@ def distribute_dat_to_bucket(dat_entry, buckets, bucket_sizes, base_dir="."):
     heapq.heappush(bucket_sizes, (new_size, smallest_bucket_index))
 
 
-def redistribute_files_dat(files_dat_file, n_buckets, max_entries=None):
+def redistribute_files_dat(files_dat_file, n_buckets, max_entries=None, show_stats=False):
     """
     Read and process each entry from the dat file and distribute them into buckets.
     """
@@ -67,12 +67,19 @@ def redistribute_files_dat(files_dat_file, n_buckets, max_entries=None):
     bucket_heap = [(0, i) for i in range(n_buckets)]
     heapq.heapify(bucket_heap)  # Turn the list into a heap
 
-    for entry in with_progress(dat_entries):
-        dat_entry = entry.strip()
+    dat_entries = [entry.strip() for entry in dat_entries]
+    entry_sizes = [(entry, get_entry_size(base_dir, entry)) for entry in with_progress(dat_entries)]
+    entry_sizes = sorted(entry_sizes, key=lambda e: e[1], reverse=True)
+
+    for dat_entry, size in entry_sizes:
         try:
-            distribute_dat_to_bucket(dat_entry, buckets, bucket_heap, base_dir)
+            distribute_dat_to_bucket(dat_entry, size, buckets, bucket_heap, base_dir)
         except Exception as e:
             raise RuntimeError(f"Error processing dat entry {dat_entry}") from e
+
+    if show_stats:
+        rank_sizes = [bucket[0] for bucket in bucket_heap]
+        show_histogram(rank_sizes)
 
     return buckets, metadata
 
@@ -96,6 +103,13 @@ def write_dat_file(buckets, infos: dict, output_file="rebalanced-files.dat"):
                     out.write(entry)
 
 
+def get_entry_size(base_dir, dat_entry):
+    """Obtain the file size of a dat entry"""
+    dat_file = f"{dat_entry}_2.dat"
+    file_path = os.path.join(base_dir, dat_file)
+    return os.path.getsize(file_path)
+
+
 def with_progress(elements):
     """A quick and easy generator for displaying progress while iterating"""
     total_elems = len(elements)
@@ -105,6 +119,17 @@ def with_progress(elements):
         if i % report_every == 0:
             print(f"{i:10} [{i*100/total_elems:3.0f}%]", file=sys.stderr)
         yield elem
+
+
+def show_histogram(rank_buckets, n_bins=50):
+    """A simple histogram CLI visualizer"""
+    logging.info("Histogram of the ranks sizes")
+    freq, bins = numpy.histogram(rank_buckets, bins=n_bins)
+    bin_start = bins[0]
+    for count, bin_end in zip(freq, bins[1:]):
+        if count:
+            print(f"[{bin_start/1204/1024:5.0f} - {bin_end/1204/1024:5.0f}]: {count:0d}")
+        bin_start = bin_end
 
 
 def main():
@@ -142,11 +167,20 @@ def main():
         action='store_true',
         help="Enable verbose output for debugging."
     )
+    parser.add_argument(
+        '--histogram',
+        action='store_true',
+        help="Additionally display the histogram of the ranks accumulated sizes"
+    )
 
     args = parser.parse_args()
 
     logging_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=logging_level, format="%(levelname)s :: %(message)s")
+
+    if args.histogram:
+        global numpy
+        import numpy
 
     if not os.path.isfile(args.input_file):
         logging.error("Input file could not be found!")
@@ -155,7 +189,9 @@ def main():
         logging.info(f"Reading from input file: {args.input_file}")
 
     # Do the redistribution
-    buckets, infos = redistribute_files_dat(args.input_file, args.n_ranks, args.max_entries)
+    buckets, infos = redistribute_files_dat(
+        args.input_file, args.n_ranks, args.max_entries, args.histogram
+    )
 
     # Create a new files.dat according to the new buckets
     write_dat_file(buckets, infos, args.output_file)
